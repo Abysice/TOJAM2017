bc7e85998d15aad035b59f3df3e1e442d1101eb5
Graphic <graphic@SOD-SJH-520-27M.local>
2017-05-06 12:49:05 -0400

    added some stuff

--- Assets/Development/Scripts/Managers/LibraryManager.cs
+++ Assets/Development/Scripts/Managers/LibraryManager.cs
@@ -1,10 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class LibraryManager : MonoBehaviour {
 
 	private GameObject m_level;
 
+	private List<GameObject> m_bookshelves = new List<GameObject>();
+	private List<Vector3> m_spawnPoints = new List<Vector3> ();
 
 	// Use this for initialization
 	void Start () {
@@ -17,11 +20,26 @@ public class LibraryManager : MonoBehaviour {
 	}
 
 
-
-
 	public void SpawnLevel(){
+		//spawn the level
 		m_level = GameObject.Instantiate(Managers.GetInstance().GetGameProperties().LevelPrefab);
+	
+		//get the book spawn points
+		for(int i = 0; i < m_level.transform.GetChildCount(); i++)
+		{
+			Transform child = m_level.transform.GetChild(i);
+			if (child.name == "SPAWN") {
+				m_spawnPoints.Add (child.position);
+			}
+		}
 
+		//spawn bookshelves
+		foreach (Vector3 pos in m_spawnPoints) {
+			GameObject temp = GameObject.Instantiate (Managers.GetInstance ().GetGameProperties ().BookShelves [0], pos, Quaternion.identity) as GameObject;
+			m_bookshelves.Add (temp);
+
+		}
+	
 	}
 
 
