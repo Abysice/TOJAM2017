5a7fa29f06080f3e2606b0fddc57e4df1be4a066
Graphic <graphic@SOD-SJH-520-27M.local>
2017-05-06 11:06:31 -0400

    stuff from me

--- Assets/Development/Scripts/Managers/Managers.cs
+++ Assets/Development/Scripts/Managers/Managers.cs
@@ -6,20 +6,33 @@ public class Managers : MonoBehaviour {
 
 	private static Managers m_instance;
 
+	private GameStateManager m_gamestatemanager;
+
+
 	//Accessors
 	public static Managers GetInstance()
 	{
 		return m_instance;
 	}
 
-
+	public GameStateManager GetGameStateManager(){
+		return m_gamestatemanager;
+	}
 
 
 	//Public Variables
-	public void Start()
+	public void Awake()
 	{
-		//initialize Managers here
+		//Create managers here
+		m_gamestatemanager = gameObject.AddComponent<GameStateManager>();
 
+
+
+
+
+
+		//preferably call init after
+		m_gamestatemanager.Init();
 	}
 
 }
